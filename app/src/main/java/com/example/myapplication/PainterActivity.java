5a4259b3f36e61246541b4a933ca0f72a144bab1
NguyenHieu201 <hieu.nguyenminh2012001@gmail.com>
2022-08-29 01:40:16 +0700

    New feature

--- app/src/main/java/com/example/myapplication/PainterActivity.java
+++ app/src/main/java/com/example/myapplication/PainterActivity.java
@@ -6,13 +6,25 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.slider.Slider;
 
 public class PainterActivity extends GeneralActivity {
 
     private ImageView imageView;
+    private Slider strokeWidthSlider;
+    private boolean isDrawText = false;
+    private FrameLayout layout;
 
     Bitmap bitmapMaster;
     Bitmap tempBitmap;
@@ -29,8 +41,12 @@ public class PainterActivity extends GeneralActivity {
         setContentView(R.layout.activity_painter);
 
         imageView = findViewById(R.id.painter_image_view);
+        strokeWidthSlider = findViewById(R.id.stroke_width_slider);
         Button applyChangeButton = findViewById(R.id.apply_change_button);
         Button cancelButton = findViewById(R.id.cancel_button);
+        Button addTextButton = findViewById(R.id.add_text_button);
+        layout = findViewById(R.id.painter_image_layout);
+
 
         paintDraw = new Paint();
         paintDraw.setStyle(Paint.Style.FILL);
@@ -90,7 +106,21 @@ public class PainterActivity extends GeneralActivity {
             finish();
         });
 
-        cancelButton.setOnClickListener(v -> finish());
+        cancelButton.setOnClickListener(v -> {
+            finish();
+        });
+
+        addTextButton.setOnClickListener(v -> {
+            EditText editText = new EditText(this);
+            editText.setWidth(1000);
+            editText.setHeight(1000);
+            editText.setText("HIEU");
+            editText.setTextSize(60);
+            editText.setTextColor(Color.BLACK);
+            RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT, RelativeLayout.LayoutParams.WRAP_CONTENT);
+            layout.addView(editText, layout.getChildCount(), layoutParams);
+            isDrawText = true;
+        });
     }
 
     private void drawOnProjectedBitMap(ImageView iv, Bitmap bm,
@@ -99,15 +129,31 @@ public class PainterActivity extends GeneralActivity {
             //outside ImageView
             float ratioWidth = (float) bm.getWidth() / (float) iv.getWidth();
             float ratioHeight = (float) bm.getHeight() / (float) iv.getHeight();
-
-            canvasMaster.drawLine(
-                    x0 * ratioWidth,
-                    y0 * ratioHeight,
-                    x * ratioWidth,
-                    y * ratioHeight,
-                    paintDraw);
+            if (isDrawText) {
+                paintDraw.setTextSize(50);
+                paintDraw.setColor(Color.BLACK);
+                paintDraw.setUnderlineText(false);
+                paintDraw.setAntiAlias(true);
+                canvasMaster.drawText("HIEU", x0 * ratioWidth, y0 * ratioHeight, paintDraw);
+            } else {
+                paintDraw.setStrokeWidth(strokeWidthSlider.getValue());
+                canvasMaster.drawLine(
+                        x0 * ratioWidth,
+                        y0 * ratioHeight,
+                        x * ratioWidth,
+                        y * ratioHeight,
+                        paintDraw);
+            }
             imageView.invalidate();
         }
+    }
+
+    private void setStrokeWidthDrawPaint(float strokeWidth) {
+        paintDraw.setStrokeWidth(strokeWidth);
+    }
+
+    private void setColorDrawPaint(int color) {
+        paintDraw.setColor(color);
     }
 
 }
